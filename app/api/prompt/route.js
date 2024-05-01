6244de99f8f815da0423ffaf09068fc4fad8aac8
Bilal <belkhoul@uoguelph.ca>
2024-05-01 19:06:31 -0400

    edited prompt route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,6 +6,10 @@ export const GET = async (req, res) => {
         await connectToDB();
         
         const prompts = await Prompt.find({}).populate('creator')
+        res.setHeader(
+            "Cache-Control",
+            "no-cache, no-store, max-age=0, must-revalidate"
+          );
         return new Response(JSON.stringify(prompts), {status:200})
     } catch (error) {
         return new Response("Failed to fetch all prompts", {status:500})
